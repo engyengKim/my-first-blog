a9cc92c7183766d1e9ed4d2bcce08981ae774583
engyengKim <engyeng9797@gmail.com>
2019-01-14 12:22:26 -0800

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
 
 # 장고가 정규표현식에 매칭되는 url을 찾아 뷰로 전달
