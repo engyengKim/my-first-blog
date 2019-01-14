060b1a990bcb24b4356826858c947ccff36c1b54
engyengKim <engyeng9797@gmail.com>
2019-01-14 10:29:41 -0800

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
+
+# 장고가 정규표현식에 매칭되는 url을 찾아 뷰로 전달
